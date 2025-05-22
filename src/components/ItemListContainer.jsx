67ba9c17e654047c359de59ba1d08e08ed49c7f8
Criso <68233846+crisoalmoniga@users.noreply.github.com>
2025-05-22 20:54:38 -0300

    Detalles corregidos

--- src/components/ItemListContainer.jsx
+++ src/components/ItemListContainer.jsx
@@ -4,7 +4,7 @@ export default function ItemListContainer({ greeting }) {
   return (
     <div className="container mt-4">
       <h2>{greeting}</h2>
-      {/* Aquí irá tu catálogo de productos */}
+      {/* Aquí irá el catálogo de productos */}
     </div>
   );
 }
